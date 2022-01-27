c455e37699c563f637b0378250f1014c6ef11efa
Tyrone <natiboigaming@gmail.com>
2022-01-27 11:39:48 -0500

    midlabs

---
+++ index.js
@@ -0,0 +1,28 @@
+function createParagraph(){
+    var word1 = document.getElementById('word1').value
+    var word2 = document.getElementById('word2').value
+    var word3  = document.getElementById('word3').value
+    var word4  = document.getElementById('word4').value
+    var word5  = document.getElementById('word5').value
+    var word6  = document.getElementById('word6').value
+    var word7  = document.getElementById('word7').value
+    var word8  = document.getElementById('word8').value
+    var word9  = document.getElementById('word9').value
+    var word10  = document.getElementById('word10').value
+    var word11  = document.getElementById('word11').value
+    var word12  = document.getElementById('word12').value
+    var word13  = document.getElementById('word13').value
+    var word14  = document.getElementById('word14').value
+    var word15  = document.getElementById('word15').value
+    var word16  = document.getElementById('word16').value
+    var word17  = document.getElementById('word17').value
+    var word18  = document.getElementById('word18').value
+    var word19  = document.getElementById('word19').value
+    var word20  = document.getElementById('word20').value
+    var word21  = document.getElementById('word21').value
+ 
+
+    var paragraph = 'A '+ word1 +' in '+ word2 +' was arrested this morning after he '+ word3 +' a in front of '+ word4 +' , but no one- not even his '+ word5 +"  -ever imagined he'd "+ word6 +'  with a '+ word7 +'  stuck in his '+ word8 +' . I always thought he was '+ word9 +" , but I never thought he'd do something like this. Even his "+ word10 +'  was surpirsed. After a brief '+ word11 +' , cops followed him to a '+ word12 +' , where he reportedly '+ word13 +' in the fry machine. In '+ word14 +' , a women was charged with a similar crime. But rather than '+ word15 +'  with a '+ word16 +' , she '+ word17 +'  with a '+ word18 +'  dog. Either way, we imagine that after witnessing him '+ word19 +'  with a '+ word20 +'  there are probably a whole lot of '+ word21 +'  that are going to need some therapy.'
+    
+   document.getElementById('answer').innerHTML = paragraph;
+}
